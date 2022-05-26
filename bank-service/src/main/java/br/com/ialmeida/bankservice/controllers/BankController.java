9c048da6a9659884d41c27bf9b1708294348f8da
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-05-26 10:23:29 -0300

    Bank Service - Creating Bank Controller

---
+++ bank-service/src/main/java/br/com/ialmeida/bankservice/controllers/BankController.java
@@ -0,0 +1,24 @@
+package br.com.ialmeida.bankservice.controllers;
+
+import br.com.ialmeida.bankservice.clients.BankClient;
+import br.com.ialmeida.bankservice.entities.Bank;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/banks")
+public class BankController {
+
+    private final BankClient bankClient;
+
+    public BankController(BankClient bankClient) {
+        this.bankClient = bankClient;
+    }
+
+    @GetMapping(value = "/{code}")
+    public Bank getAddressByCode(@PathVariable String code) {
+        return bankClient.getAddressByCode(code);
+    }
+}
