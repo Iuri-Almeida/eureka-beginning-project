7ce76fb391b9b9b97b2e51e0e7d6742e46447250
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-05-26 10:20:29 -0300

    Bank Service - Adding FeignClient to project

---
+++ bank-service/src/main/java/br/com/ialmeida/bankservice/clients/BankClient.java
@@ -0,0 +1,14 @@
+package br.com.ialmeida.bankservice.clients;
+
+import br.com.ialmeida.bankservice.entities.Bank;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(url = "https://brasilapi.com.br/api/banks/v1", name = "bank")
+public interface BankClient {
+
+    @GetMapping(value = "/{code}")
+    Bank getAddressByCode(@PathVariable String code);
+
+}
