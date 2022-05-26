01fbd20e4290009ff54002cccfe0cd205aaa9a79
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-05-26 11:14:51 -0300

    Adding Resilience4J to project

--- person-service/src/main/java/br/com/ialmeida/personservice/clients/AddressClient.java
+++ person-service/src/main/java/br/com/ialmeida/personservice/clients/AddressClient.java
@@ -1,6 +1,7 @@
 package br.com.ialmeida.personservice.clients;
 
 import br.com.ialmeida.personservice.entities.Address;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface AddressClient {
 
     @GetMapping(value = "/address/{cep}")
+    @CircuitBreaker(name = "service")
     Address getAddressByCEP(@PathVariable String cep);
 
 }
--- person-service/src/main/java/br/com/ialmeida/personservice/clients/BankClient.java
+++ person-service/src/main/java/br/com/ialmeida/personservice/clients/BankClient.java
@@ -1,6 +1,7 @@
 package br.com.ialmeida.personservice.clients;
 
 import br.com.ialmeida.personservice.entities.Bank;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface BankClient {
 
     @GetMapping(value = "/banks/{code}")
+    @CircuitBreaker(name = "service")
     Bank getBankByCode(@PathVariable Long code);
 
 }
